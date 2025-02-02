8d27b3e335fd1bc30eb841819c4193dd77a144e7
shrutya22487 <shrutya22487@iiitd.ac.in>
2025-02-02 16:32:00 +0530

    update

--- task2.py
+++ task2.py
@@ -1,6 +1,7 @@
+import random
 import torch
 import torch.nn as nn
-import torch.optim as optim
+import torch.optim
 from torch.ao.nn.quantized import Dropout
 from torch.nn.functional import dropout
 from torch.utils.data import Dataset, DataLoader, random_split
@@ -12,38 +13,51 @@ from tqdm import tqdm
 import torch.nn.functional
 import task1
 
-vocabulary_size = 5000
+################################# HYPERPARAMETERS #########################################
+vocabulary_size = 14000
 context_window = 2
 embedding_dim = 500
 batch_size = 1024
 epochs = 40
 lr = 0.001
-dropout_rate = 0
+dropout_rate = 0.05
 word_index_mapping, index_word_mapping = {}, {}
 train_dataloader = []
-
 val_dataloader = []
 
+
+################################# DATASET LOADING #########################################
+
 class Word2VecDataset(Dataset):
+    """"
+    Is responsible for loading the dataset and provides functions to load, process and get items at a specific index
+    """
+    # Reference -> https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+    # Initialize
     def __init__(self, corpus):
         self.preprocessed_data = []
         self.corpus = corpus
 
+    # returns the number of items in data
     def __len__(self):
         return len(self.preprocessed_data)
 
+    # gets the item at a particular index and returns the target and context tensor after performing padding
+    # note that padding is done using the [PAD] token generated in task 1
     def __getitem__(self, index):
-        target, context = self.preprocessed_data[index]
-        context_tensor = torch.tensor(context, dtype=torch.long)
+        target = self.preprocessed_data[index][0]
+        context = torch.tensor(self.preprocessed_data[index][1], dtype=torch.long)
 
-        total_padding = 2 * context_window - len(context_tensor)
+        total_padding = 2 * context_window - len(context)
 
         left_padding = total_padding // 2
         right_padding = total_padding - left_padding
 
-        context = torch.nn.functional.pad(context_tensor, (left_padding, right_padding), value=0)
+        # Reference -> https://pytorch.org/docs/stable/nn.functional.html
+        context = torch.nn.functional.pad(context, (left_padding, right_padding), value=0)
         return torch.tensor(target, dtype=torch.long), context
 
+    # finds context words for all the words in the corpus
     def preprocess_data(self, word_index_mapping):
         data = []
         for key in self.corpus:
@@ -54,9 +68,10 @@ class Word2VecDataset(Dataset):
             for i in range(0, n):
                 target = tokens[i]
 
-                # if target not in word_index_mapping:
-                #     print(f"Warning: '{target}' not found in vocabulary, skipping.")
-                #     continue  # Skip this word
+                # was giving for some words randomly thats why I added
+                if target not in word_index_mapping:
+                    print(target,"not found in vocabulary")
+                    continue
 
                 tokens_before_word = tokens[max(0, i - context_window):i]
                 tokens_after_word = tokens[i + 1: min(n + 1, i + 1 + context_window)]
@@ -65,6 +80,7 @@ class Word2VecDataset(Dataset):
 
                 target_index = word_index_mapping[target]
                 context_index = []
+
                 for word in context:
                     if word in word_index_mapping:
                         context_index.append(word_index_mapping[word])
@@ -74,36 +90,44 @@ class Word2VecDataset(Dataset):
 
         self.preprocessed_data = data
 
+################################# MODEL #########################################
 
 class Word2VecModel(nn.Module):
+    # Reference -> https://pytorch.org/tutorials/beginner/basics/buildmodel_tutorial.html
+    # Make the layers and Initilaize them
     def __init__(self, vocab_size):
         super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.dropout = nn.Dropout(dropout_rate)
-        self.linear = nn.Linear(embedding_dim, vocab_size)
 
-        nn.init.xavier_uniform_(self.embedding.weight)
-        nn.init.xavier_uniform_(self.linear.weight)
-        if self.linear.bias is not None:
-            nn.init.zeros_(self.linear.bias)
+        self.network = nn.Sequential(
+            nn.Embedding(vocab_size, embedding_dim),
+            nn.Dropout(dropout_rate),
+            nn.Linear(embedding_dim, vocab_size)
+        )
 
+        nn.init.xavier_uniform_(self.network[0].weight)
+        nn.init.xavier_uniform_(self.network[2].weight)
+        if self.network[2].bias is not None:
+            nn.init.zeros_(self.network[2].bias)
+
+    # pushes the data forward to make predictions
     def forward(self, context):
-        embedded = self.embedding(context)  # [batch_size, context_length, embedding_dim]
-        aggregated = embedded.mean(dim=1)     # [batch_size, embedding_dim]
-        aggregated = self.dropout(aggregated)   # Apply dropout
-        out = self.linear(aggregated)           # [batch_size, vocab_size]
+        embedded = self.network[0](context).mean(dim = 1)
+        aggregated = self.network[1](embedded)
+        out = self.network[2](aggregated)
         return out
 
+    # Main training loop
     def train_model(self, model, criterion, optimizer):
         loss_list, val_loss = [], []
-
+        # Reference -> https://pytorch.org/tutorials/beginner/basics/autogradqs_tutorial.html
         for _ in tqdm(range(epochs)):
             total_loss = 0
-            model.train()  # enable this if we r able to implement some dropout thingy
+            model.train()  # enable this if we r able to implement some dropout thingy, this enables the training capability of the model
 
             for target, context in train_dataloader:
-                target = target.long()
-                context = context.long()
+
+                # target = target.long()
+                # context = context.long()
 
                 pred = model.forward(context)
                 loss = criterion(pred, target)
@@ -119,7 +143,7 @@ class Word2VecModel(nn.Module):
             loss_list.append(avg_loss)
 
             total_loss = 0
-            model.eval()
+            model.eval() # disable sthe dropout
 
             with torch.no_grad():
                 for target, context in val_dataloader:
@@ -135,83 +159,76 @@ class Word2VecModel(nn.Module):
 
         return loss_list, val_loss
 
-    # def get_triplets(self, model):
-    #     embeddings = model.network[0].weight.data.cpu().numpy()
-    #     similarities = cosine_similarity(embeddings)
-    #     triplets = []
-    #
-    #     for item in word_index_mapping:
-    #         word, index = item[0], item[1]
-    #
-    #         similar, triplet = [], []
-    #         similar_indices = np.argsort(similarities[index])[::-1]
-    #         similar_indices = similar_indices[:3]
-    #
-    #         for i in similar_indices:
-    #             similar.append(index_word_mapping[i])
-    #         del similar[index_word_mapping[index]]
-    #
-    #         dissimilar_index = np.argsort(similarities[idx])[0]
-    #         dissimilar = index_word_mapping[dissimilar_index]
-    #
-    #         triplet = [word, similar, dissimilar]
-    #         triplets.append(triplet)
-    #
-    #     for triplet in triplets:
-    #         print("Word:", triplet[0])
-    #         print("Similar:", triplet[1])
-    #         print("Dissimilar:", triplet[2])
-    #         print()
+
     def get_triplets(self):
-        embeddings = self.embedding.weight.data.cpu().numpy()
-        similarities = cosine_similarity(embeddings)
 
+        """"
+        Will generate 5 random triplets and show the similarities as well
+        remember to reound them off
+        """
+        embeddings = self.network[0].weight.data.cpu().numpy()
+
+        #Reference -> https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.cosine_similarity.html
+        cos_similarity_mat = cosine_similarity(embeddings)
+        indexes = []
+        for i in range(5):
+            indexes.append(random.randint(1, len(word_index_mapping)))
         triplets = []
-
         for word, index in word_index_mapping.items():
-            similar = []
+            if index in indexes:
+                similar, similar_indices_list = [], []
 
-            similar_indices = np.argsort(similarities[index])[::-1]
-            similar_indices = [i for i in similar_indices if i != index][:3]
+                similar_indices = np.argsort(cos_similarity_mat[index])[::-1]
 
-            for i in similar_indices:
-                similar.append((index_word_mapping[i], similarities[index][i]))
+                for i in similar_indices:
+                    if i != index:
+                        similar_indices_list.append(i)
 
-            dissimilar_index = np.argsort(similarities[index])[0]
-            dissimilar = (index_word_mapping[dissimilar_index], similarities[index][dissimilar_index])
+                similar_indices = similar_indices_list[:3]
 
-            triplets.append([word, similar, dissimilar])
+                for i in similar_indices:
+                    similar.append([index_word_mapping[i], cos_similarity_mat[index][i]])
+
+                dissimilar_index = np.argsort(cos_similarity_mat[index])[0]
+                dissimilar = (index_word_mapping[dissimilar_index], cos_similarity_mat[index][dissimilar_index])
+
+                triplet = [word, similar, dissimilar]
+                triplets.append(triplet)
 
         for triplet in triplets:
-            print("Word:", triplet[0])
-            print("Similar:", [(w, round(sim, 4)) for w, sim in triplet[1]])
-            print("Dissimilar:", (triplet[2][0], round(triplet[2][1], 4)))
-            print()
+            print(triplet[0], "\n")
+            print("similar words:")
+            for i in range(len(triplet[1])):
+                print("word: " ,triplet[1][i][0]," ", "with similarity: ", triplet[1][i][1])
+            print("Dissimilar:", triplet[2][0], triplet[2][1] , "\n")
 
-        return triplets
-
-
-def get_data(vocab_size, split=0.9):
+# main data function, loads data from files and invokes the dataloader
+def get_data(vocab_size, split=0.85):
     task1.make_vocab_and_tokenize(vocab_size)
 
     file_path = "tokenized_data.json"
-    with open(file_path, "r", encoding="utf-8") as file:
-        corpus = json.load(file)
+    with open(file_path, "r", encoding="utf-8") as f:
+        corpus = json.load(f)
 
     vocab_file_path = "vocabulary_86.txt"
 
     with open(vocab_file_path, "r", encoding="utf-8") as f:
-        vocab = [line.strip() for line in f]
+        vocab = []
+        for line in f:
+            vocab.append(line.strip())
+    word_index_mapping,index_word_mapping = {},{}
 
-    word_index_mapping = {word: idx for idx, word in enumerate(vocab)}
-    index_word_mapping = {idx: word for word, idx in word_index_mapping.items()}
+    for i, j in enumerate(vocab):
+        word_index_mapping[j] = i
+    for i, j in word_index_mapping.items():
+        index_word_mapping[j] = i
+
     dataset = Word2VecDataset(corpus)
     dataset.preprocess_data(word_index_mapping)
 
     n = len(dataset)
     train = int(n * split)
     val = n - train
-
     train_dataset, val_dataset = random_split(dataset, [train, val])
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -221,6 +238,9 @@ def get_data(vocab_size, split=0.9):
 
 
 def plot(loss_list, val_loss):
+    """
+    Helper function to just plot the graphs
+    """
     plt.plot(range(1, epochs + 1), loss_list, label="Training Loss")
     plt.plot(range(1, epochs + 1), val_loss, label="Validation Loss")
     plt.xlabel("Epochs")
@@ -231,40 +251,56 @@ def plot(loss_list, val_loss):
     plt.grid(visible=True)
     plt.show()
 
-def get_triplet_for_word(self, word):
-    if word not in word_index_mapping:
-        print(f"Word '{word}' not found in vocabulary.")
-        return None
+def get_triplet_for_word(model,word):
 
-    index = word_index_mapping[word]
-    embeddings = self.embedding.weight.data.cpu().numpy()
-    similarities = cosine_similarity(embeddings)
+    """"
+    to find triplets for a specific word
+    """
 
-    # Get sorted indices of words based on similarity (descending order)
-    similar_indices = np.argsort(similarities[index])[::-1]
 
-    # Exclude the word itself (index = `index`)
-    similar_indices = [i for i in similar_indices if i != index][:3]
+    index_of_word = word_index_mapping[word]
+    embeddings = model.network[0].weight.data.cpu().numpy()
 
-    similar_words = [(index_word_mapping[i], similarities[index][i]) for i in similar_indices]
+    #Reference -> https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.cosine_similarity.html
 
-    # Find the most dissimilar word (smallest similarity score)
-    dissimilar_index = np.argsort(similarities[index])[0]  # Least similar word
-    dissimilar_word = index_word_mapping[dissimilar_index]
-    dissimilar_similarity = similarities[index][dissimilar_index]
+    cos_similarity_mat = cosine_similarity(embeddings)
 
-    # Print results
-    print(f"Word: {word}")
-    print("Similar Words (with Cosine Similarity):")
-    for sim_word, sim_value in similar_words:
-        print(f"  {sim_word}: {sim_value:.4f}")
+    triplets = []
 
-    print(f"Dissimilar Word: {dissimilar_word} (Cosine Similarity: {dissimilar_similarity:.4f})\n")
+    similar_indices = np.argsort(cos_similarity_mat[index_of_word])[::-1]
+    similar_indices_list, similar = [], []
 
-    return word, similar_words, (dissimilar_word, dissimilar_similarity)
+    for i in similar_indices:
+        if i != index_of_word:
+            similar_indices_list.append(i)
+
+    similar_indices = similar_indices_list[:4]
+
+    for i in similar_indices:
+        similar.append([index_word_mapping[i], cos_similarity_mat[index_of_word][i]])
+
+    dissimilar_index = np.argsort(cos_similarity_mat[index_of_word])[0]
+    dissimilar = (index_word_mapping[dissimilar_index], cos_similarity_mat[index_of_word][dissimilar_index])
+
+    triplet = [word, similar, dissimilar]
+    triplets.append(triplet)
+
+    for triplet in triplets:
+        print(triplet[0])
+        print("similar words:")
+        for i in range(len(triplet[1])):
+            print(triplet[1][i][0], "with similarity: ", triplet[1][i][1])
+        print("Dissimilar:", triplet[2][0], triplet[2][1])
 
 def run_Word2Vec(vocabulary_size_ = 14000, context_window_ = 2,embedding_dim_ = 400,batch_size_ = 1024,epochs_ = 50,lr_ = 0.001, dropout_rate_ = 0.3):
 
+    """"
+    Pipelining functions
+    1. makes the vocabulary and tokenized dataset from task1
+    2. makes the dataset
+    3. trains and saves and evaluates the model
+    4. gives triplets for similarity and dissimilarities
+    """
     global train_dataloader, val_dataloader, word_index_mapping, index_word_mapping, vocabulary_size, context_window, embedding_dim, batch_size, epochs, lr, dropout_rate
 
     vocabulary_size = vocabulary_size_
@@ -280,7 +316,9 @@ def run_Word2Vec(vocabulary_size_ = 14000, context_window_ = 2,embedding_dim_ = 
 
     model = Word2VecModel(vocabulary_size)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
+
+    #Reference -> https://pytorch.org/docs/stable/optim.html
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
 
     print("Training...")
 
@@ -290,10 +328,12 @@ def run_Word2Vec(vocabulary_size_ = 14000, context_window_ = 2,embedding_dim_ = 
     torch.save(model.state_dict(), "word2vec_checkpoint.pth")
 
     model.get_triplets()
-
+    #
     get_triplet_for_word(model, "happy")
     get_triplet_for_word(model, "sad")
     get_triplet_for_word(model, "punish")
+    get_triplet_for_word(model, "politics")
+    get_triplet_for_word(model, "him")
 
 if __name__ == "__main__":
     run_Word2Vec( vocabulary_size_= vocabulary_size, context_window_= context_window, embedding_dim_= embedding_dim, batch_size_= batch_size, epochs_= epochs, lr_= lr, dropout_rate_=dropout_rate)
